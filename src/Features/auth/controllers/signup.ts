70788ba1ebad2aec8eae59b97f0297fb769edf13
Kasoma Ibrahim <82798823+akibrahimug@users.noreply.github.com>
2023-07-02 16:28:18 +0100

    Setting up the user interface and schema

--- src/Features/auth/controllers/signup.ts
+++ src/Features/auth/controllers/signup.ts
@@ -2,10 +2,13 @@ import { ObjectId } from 'mongodb';
 import { Request, Response } from 'express';
 import { joiValidation } from '@global/decorators/joi-validation.decorator';
 import { signupSchema } from '@auth/schemes/signup';
-import { IAuthDocument } from '@auth/interfaces/auth.interface';
+import { IAuthDocument, ISignUpData } from '@auth/interfaces/auth.interface';
 import { authService } from '@service/db/auth.service';
 import { BadRequestError } from '@global/helpers/error-handler';
-
+import { Helpers } from '@global/helpers/helpers';
+import { uploads } from '@global/helpers/cloudinary-upload';
+import { UploadApiResponse } from 'cloudinary';
+import HTTP_STATUS from 'http-status-codes';
 export class Signup {
   @joiValidation(signupSchema)
   public async create(req: Request, res: Response): Promise<void> {
@@ -16,5 +19,46 @@ export class Signup {
     if (checkIfUserExist) {
       throw new BadRequestError('Invalid credentials');
     }
+
+    const authObjectId: ObjectId = new ObjectId();
+    const userObjectId: ObjectId = new ObjectId();
+    const uId = `${Helpers.getRandomintegers(12)}`;
+    const authData: IAuthDocument = Signup.prototype.signupData({
+      _id: authObjectId,
+      uId,
+      username,
+      password,
+      email,
+      avatarColor,
+    });
+
+    const result: UploadApiResponse = (await uploads(
+      avatarImage,
+      `${userObjectId}`,
+      true,
+      true
+    )) as UploadApiResponse;
+    if (!result?.public_id) {
+      console.log('Result>>>>>>>>>>>>>>>', result);
+      console.log('UESROBJECT>>>>>>>>>>>>>>>', `${userObjectId}`);
+      throw new BadRequestError('File upload: Error occurred. Try again');
+    }
+
+    res
+      .status(HTTP_STATUS.CREATED)
+      .json({ message: 'User created succesefully', authData });
+  }
+
+  private signupData(data: ISignUpData): IAuthDocument {
+    const { _id, username, email, password, avatarColor, uId } = data;
+    return {
+      _id,
+      uId,
+      username: Helpers.firstLetterUppercase(username),
+      email: Helpers.lowercase(email),
+      password,
+      avatarColor,
+      createdAt: new Date(),
+    } as IAuthDocument;
   }
 }
--- src/Shared/globals/Helpers/helpers.ts
+++ src/Shared/globals/Helpers/helpers.ts
@@ -13,4 +13,15 @@ export class Helpers {
   static lowercase(str: string): string {
     return str.toLowerCase();
   }
+
+  static getRandomintegers(intergerLength: number): number {
+    const characters = '0123456789';
+    let result = ' ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < intergerLength; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return parseInt(result, 10);
+  }
 }
