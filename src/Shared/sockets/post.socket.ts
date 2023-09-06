1a3ba9f9fec5d616478dbb42e81fceb39dcbd48b
kasoma <kasomaibrahim@gmail.com>
2023-09-06 21:47:37 +0100

    feat: implemented post comment feature intial set up

--- src/Shared/sockets/post.socket.ts
+++ src/Shared/sockets/post.socket.ts
@@ -1,3 +1,5 @@
+import { ICommentDocument } from '@comment/interfaces/comments.interface';
+import { IReactionDocument } from '@reaction/interfaces/reaction.interface';
 import { Server, Socket } from 'socket.io';
 
 export let socketIOPostObject: Server;
@@ -11,6 +13,14 @@ export class SocketIoPostHandler {
 
   public listen(): void {
     this.io.on('connection', (socket: Socket) => {
+      // reaction
+      socket.on('reaction', (reaction: IReactionDocument) => {
+        this.io.emit('Update like', reaction);
+      });
+      // comment
+      socket.on('comment', (comment: ICommentDocument) => {
+        this.io.emit('Update comment', comment);
+      });
       console.log('Post socket handler');
     });
   }
