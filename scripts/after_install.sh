8d34727c5d3313675592029f994701956bc75575
kasoma <kasomaibrahim@gmail.com>
2024-05-23 19:52:25 +0100

    AWS-SET-UP/adding the code deploy

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,5 +7,5 @@ sudo rm -rf .env.develop
 aws s3 sync s3://talkieappserver-env-files/develop .
 unzip env-file.zip
 sudo cp .env.develop .env
-sudo pm2 delete all
-sudo yarn
+pm2 delete all
+yarn
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/ec2-user/talkie-backend
-sudo yarn build
-sudo yarn start
+yarn build
+yarn start
