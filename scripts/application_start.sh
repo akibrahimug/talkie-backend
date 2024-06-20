9f0edf3556822c8eb3fda3d7b64ea4b9aa8280a2
kasoma <kasomaibrahim@gmail.com>
2024-06-20 06:39:47 +0100

    AWS-SET-UP/adding the code deploy

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 cd /home/ec2-user/talkie-backend
+echo "installing NPM >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+export PATH=~/.npm-global/bin:$PATH
+source ~/.bashrc
+if [ $(program_is_installed yarn) == 0 ]; then
+echo "installing YARN >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "
+npm install --global yarn
+fi
 sudo chown -R ec2-user:ec2-user /home/ec2-user/talkie-backend
 yarn build
 yarn start
