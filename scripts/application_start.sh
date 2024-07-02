830b675b60a6dcf1196bdff4a677ac7aca31b719
kasoma <kasomaibrahim@gmail.com>
2024-07-02 06:05:52 +0100

    refactor

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,6 +4,6 @@ cd /home/ec2-user/talkie-backend
 # Source the .bashrc file to load the environment variables
 source /home/ec2-user/.bashrc
 echo "Building updated codedeploy version >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-sudo yarn build
-sudo yarn start
+yarn build
+yarn start
 echo "Application is started >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
